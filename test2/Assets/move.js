5c5c2bba734b49505185c0060938361ea801d6b6
grigri0428 <fuji_e_saku@hotmail.com>
2012-12-17 09:07:44 +0900

    7 commit

--- test2/Assets/move.js
+++ test2/Assets/move.js
@@ -6,7 +6,7 @@ function Start () {
 
 function Update () {
 
-transform.rotation *= Quaternion.AngleAxis(Input.GetAxis("Horizontal")*30*Time.deltaTime,Vector3(0,0,1));
+transform.rotation *= Quaternion.AngleAxis(Input.GetAxis("Horizontal")*90*Time.deltaTime,Vector3(0,0,1));
 transform.rotation *= Quaternion.AngleAxis(Input.GetAxis("Vertical")*90*Time.deltaTime,Vector3(1,0,0));
 
 
